4772c4395231003eb05e778d028f55b2f1ad4f5d
yonghee kim <kyh1028@gmail.com>
2019-11-02 23:20:19 +0900

    date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    {{ version }}
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import project from '../../package.json';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      version: project.version,
+    };
+  },
 };
 </script>
